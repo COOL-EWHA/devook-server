fd3207517d947dea2d06295d92a940f9292723a9
Jungeunkim-dev <svaha@ewhain.net>
2022-01-07 14:55:09 +0900

    Initial setting

---
+++ src/main/java/com/ewha/devookserver/DevookServerApplication.java
@@ -0,0 +1,13 @@
+package com.ewha.devookserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevookServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevookServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ewha/devookserver/DevookServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewha.devookserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevookServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
