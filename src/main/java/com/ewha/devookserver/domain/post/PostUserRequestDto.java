a2d8a983dfa4bd14464c8ff9c176f8585744aec5
Jungeunkim-dev <svaha@ewhain.net>
2022-01-24 20:53:59 +0900

    chore : 북마크 추가 API RequestBody 변수명 수정
    id에서 postId로 JSON request값 변수명을 변경

--- src/main/java/com/ewha/devookserver/domain/post/PostUserRequestDto.java
+++ src/main/java/com/ewha/devookserver/domain/post/PostUserRequestDto.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class PostUserRequestDto {
-  public String id;
+  public String postId;
   public String url;
   public String memo;
 }
