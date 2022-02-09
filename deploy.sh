f66a3284ed962c5c4c86e9b921fd7552b0faf83b
Jungeunkim-dev <svaha@ewhain.net>
2022-02-10 08:33:34 +0900

    #7 fix : delete typo

--- deploy.sh
+++ deploy.sh
@@ -18,4 +18,4 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
-nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar $JAR_NAME 
