1d6eeec7867442e43dcf70a9aebded881cb51132
Jungeunkim-dev <svaha@ewhain.net>
2022-02-10 08:53:27 +0900

    #7 fix : change 8080 port

--- deploy.sh
+++ deploy.sh
@@ -11,13 +11,7 @@ CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
-if [ -z "$CURRENT_PID" ]; then
-   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-   echo "> kill -15 $CURRENT_PID"
-   kill -15 $CURRENT_PID
-   sleep 5
-fi
+fuser -k -n tcp 8080
 
 echo "> 새 어플리케이션 배포"
 
