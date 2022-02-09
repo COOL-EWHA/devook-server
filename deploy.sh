fe33f93969ee66cfdb8c321e3b5e5005acb49450
Jungeunkim-dev <svaha@ewhain.net>
2022-02-10 08:44:58 +0900

    #7 fix : change deploy.sh ver

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,26 @@
-REPOSITORY=/home/ec2-user/app
+REPOSITORY=/home/ec2-user/app/deploy
 PROJECT_NAME=devook-server
 
 echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/deploy/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/deloy/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
+
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+   echo "> kill -15 $CURRENT_PID"
+   kill -15 $CURRENT_PID
+   sleep 5
+fi
 
 echo "> 새 어플리케이션 배포"
+
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
@@ -17,5 +31,5 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
-nohup java -jar $JAR_NAME 
+nohup java -jar \
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
