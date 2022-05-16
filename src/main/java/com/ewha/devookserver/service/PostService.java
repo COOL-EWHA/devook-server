c4199ed5de3872aebd3bc0791e45012e3785c70a
Jungeunkim-dev <svaha@ewhain.net>
2022-05-17 07:51:49 +0900

    test : 이전 값과 비교 확인

--- src/main/java/com/ewha/devookserver/service/PostService.java
+++ src/main/java/com/ewha/devookserver/service/PostService.java
@@ -238,8 +238,7 @@ public class PostService {
 
     for (Post post : productList.getValues()) {
       List<String> forTestString = new ArrayList<>();
-      // List<PostTag> postTagList = queryRepository.findAllTagsByPost(post.getPostIdx().intValue());
-      List<PostTag> postTagList = queryRepository.findAllTagsByPost(Integer.parseInt(post.getPostMemo()));
+      List<PostTag> postTagList = queryRepository.findAllTagsByPost(post.getPostIdx().intValue());
 
       for (PostTag postTag : postTagList) {
         forTestString.add(postTag.getPostTagName());
@@ -282,8 +281,7 @@ public class PostService {
 
     for (Post post : productList.getValues()) {
       List<String> forTestString = new ArrayList<>();
-      //List<PostTag> postTagList = queryRepository.findAllTagsByPost(post.getPostIdx().intValue());
-      List<PostTag> postTagList = queryRepository.findAllTagsByPost(Integer.parseInt(post.getPostMemo()));
+      List<PostTag> postTagList = queryRepository.findAllTagsByPost(post.getPostIdx().intValue());
 
       for (PostTag postTag : postTagList) {
         forTestString.add(postTag.getPostTagName());
@@ -322,9 +320,7 @@ public class PostService {
 
     for (Post post : productList.getValues()) {
       List<String> forTestString = new ArrayList<>();
-
-      // List<PostTag> postTagList = queryRepository.findAllTagsByPost(post.getPostIdx().intValue());
-      List<PostTag> postTagList = queryRepository.findAllTagsByPost(Integer.parseInt(post.getPostMemo()));
+      List<PostTag> postTagList = queryRepository.findAllTagsByPost(post.getPostIdx().intValue());
 
       for (PostTag postTag : postTagList) {
         forTestString.add(postTag.getPostTagName());
